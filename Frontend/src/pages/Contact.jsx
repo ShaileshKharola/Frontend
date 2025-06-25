c56f5521b3ecebec8264c5537df9bb382f7f6a85
ShaileshKharola <shaileshkharola0048@gmail.com>
2025-06-25 11:22:48 +0530

    Update Contact.jsx

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <Title text1={'CONTACT'} text2={'US'} />
       </div>
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-20'>
-        <img className='w-full md:max-w-[480px' src={assets.contact_img} alt=""Contact Store Location"" />
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="Contact Store Location" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
           <p className='text-gray-500'>30/3 Church Flat Char Dukan Landour Cantt<br/>Mussoorie,Dehradun-248179</p>
